beffbbbf9160471bb2ab86f3c9d6055650b1a29a
Guilherme Marobo <guimarobo@gmail.com>
2025-06-10 17:51:31 -0300

    base

---
+++ banco_v1.py
@@ -0,0 +1,36 @@
+menu = """
+======================================
+    Bem-vindo ao Sistema Bancário V1
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato 
+    [q] Sair
+======================================
+
+Digite a opção desejada:
+"""
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    
+    opcao = input(menu)
+
+    if opcao == "d":
+
+    elif opcao == "s":
+
+    elif opcao == "e":
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Opção inválida, por favor tente novamente.")
+
+    
+
